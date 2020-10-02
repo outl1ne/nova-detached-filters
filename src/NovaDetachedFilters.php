21b96c63afc5b5d01588d9ee4b012f3a50434096
Kaspar Rosin <kaspar.rosin@tptlive.ee>
2020-10-02 15:14:27 +0300

    persist filter state

--- src/NovaDetachedFilters.php
+++ src/NovaDetachedFilters.php
@@ -6,9 +6,10 @@ use Laravel\Nova\Card;
 
 class NovaDetachedFilters extends Card
 {
-    public $width = '1/3'; // (1/3, 1/2, or full)
-    public $filters = [];
-    public $withReset = false;
+    public $width = '1/3'; // (full, 1/3, 1/2 etc..)
+    protected array $filters = [];
+    protected bool $withReset = false;
+    protected bool $persistFilters = false;
 
     public function __construct($filters = [])
     {
@@ -20,14 +21,30 @@ class NovaDetachedFilters extends Card
         return 'nova-detached-filters';
     }
 
-    public function withReset($value = true): bool
+    public function width($width)
     {
-        return $this->withReset = $value;
+        if (empty($width)) return $this;
+        $this->width = $width;
+        return $this;
+    }
+
+    public function withReset(bool $value = true)
+    {
+        $this->withReset = $value;
+        return $this;
+    }
+
+    public function persistFilters(bool $value = true)
+    {
+        $this->persistFilters = $value;
+        return $this;
     }
 
     public function jsonSerialize()
     {
         return array_merge(parent::jsonSerialize(), [
+            'withReset' => $this->withReset,
+            'persistFilters' => $this->persistFilters,
             'filters' => collect(is_callable($this->filters) ? $this->filters() : $this->filters)->each(function ($filter) {
                 return $filter->jsonSerialize();
             }),
